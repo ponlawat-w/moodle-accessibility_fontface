b273b346b4e17b6e46e86368554d0af055c894bf
ponlawat-w <ponlawat_w@outlook.co.th>
2024-08-24 11:40:38 +0700

    Fixed coding style for Moodle 4.4

--- lang/en/accessibility_fontface.php
+++ lang/en/accessibility_fontface.php
@@ -22,7 +22,7 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Font Face';
-$string['serif'] = 'Serif';
-$string['sansserif'] = 'Sans Serif';
 $string['dyslexic'] = 'Dyslexic';
+$string['pluginname'] = 'Font Face';
+$string['sansserif'] = 'Sans Serif';
+$string['serif'] = 'Serif';
